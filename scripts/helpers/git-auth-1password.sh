97df49620036ff565aee5f2f047882e5a9f0b748
Mark Alston <malston@vmware.com>
2025-01-12 10:14:50 -0700

    Setup op plugin

--- scripts/helpers/git-auth-1password.sh
+++ scripts/helpers/git-auth-1password.sh
@@ -1,12 +1,16 @@
-   #!/bin/bash
-   op signin --raw > /dev/null 2>&1
-   
-   case "$1" in
-       get)
-           read url
-           url=${url#url=}
-           op get item "$url" | op get totp --json --vault=GIT | jq -r '.fields[] | select(.id=="password") | .value'
-           ;;
-       store|erase)
-           ;;
-   esac
+#!/bin/bash
+# op signin --raw > /dev/null 2>&1
+case "$1" in
+    get)
+        read url
+        url=${url#url=}
+        op get item "$url" | op get totp --json --vault=GIT | jq -r '.fields[] | select(.id=="password") | .value'
+        ;;
+    store|erase)
+        ;;
+    *)
+    op signin
+    op plugin init gh
+    op plugin inspect gh
+    ;;
+esac
